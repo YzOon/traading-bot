367b39007882c0f0d296bdb001897a792c2b9632
[ Yazan ] <gmr86903@gmail.com>
2025-04-13 17:19:40 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+reply_text("مرحبًا! أنا بوت لرسائل التنبيه على العملات الحلال.")
+
+# دالة إعداد البوت
+def main():
+    # توكن البوت (استبدله بالتوكن الخاص بك)
+    updater = Updater("7948997743:AAH77y46rqNrJEJE1uk_sZJlk1f95fV7SKk", use_context=True)
+    dp = updater.dispatcher
+    
+    # إضافة معالجات الأوامر
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("add_coin", add_coin))
+    dp.add_handler(CommandHandler("remove_coin", remove_coin))
+    
+    # بدء البوت
+    updater.start_polling()
+    updater.idle()
+
+# تشغيل Flask في وضع متوازي مع البوت
+if name == '__main__':
+    from threading import Thread
+    t = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
+    t.start()
+    main()
